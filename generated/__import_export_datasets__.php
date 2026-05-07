626c04493401185d2615ae2b4be23f43427c689e
Istvan Ujj-Meszaros <istvan@radaptor.com>
2026-05-07 04:38:28 +0200

    Refresh generated descriptors

--- generated/__import_export_datasets__.php
+++ generated/__import_export_datasets__.php
@@ -3,8 +3,10 @@
 class ImportExportDatasetList
 {
 	public const string I18N_TRANSLATIONS = 'I18nTranslations';
+	public const string SITE_SNAPSHOT = 'SiteSnapshot';
 
 	protected static array $_datasetNames = [
 		'I18nTranslations',
+		'SiteSnapshot',
 	];
 }
